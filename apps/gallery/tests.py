49926bf63dd69756a67f255c92d0daa23efcd13f
Oleg Gordienko <admin@gordio.pp.ua>
2014-02-09 21:59:36 +0200

    gallery: tests: (fix) Admin Panel user tests

--- apps/gallery/tests.py
+++ apps/gallery/tests.py
@@ -8,48 +8,53 @@ class GalleryTestCase(TestCase):
 
     def setUp(self):
         self.client = Client()
-        self.user = User.objects.create(username='fakeuser',
-            password="fakepass", is_staff=False)
-        self.staff = User.objects.create(username='fakestaff',
-            password="fakepass", is_staff=True)    
-        self.admin = User.objects.create(username='fakeadmin',
-            password="fakepass", is_superuser=True)
+        self.user = User(username='fakeuser', is_staff=False)
+        self.user.set_password('pass')
+        self.user.save()
+        self.staff = User(username='fakestaff', is_staff=True)
+        self.staff.set_password('pass')
+        self.staff.save()
+        self.admin = User(username='fakeadmin', is_superuser=True)
+        self.admin.set_password('pass')
+        self.admin.save()
 
     def test_empty_page_rendering(self):
         response = self.client.get(reverse('gallery'))
 
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "No photos")
-        # TestCase.assertTemplateUsed(response, 'gallery/photo_list.html')
 
     def test_admin_panel_link(self):
-        # FIXME: How test_client_get as user?
-        # no Link for User
-        self.client.login(username=self.user.username,
-            password=self.user.password)
+        # Hide for User
+        self.client.login(username=self.user.username, password="pass")
 
         response = self.client.get(reverse('gallery'))
-        print("\n", response.context["user"], "\n")
+        self.assertEqual(response.context["user"], self.user)
         self.assertEqual(response.status_code, 200)
         self.assertNotContains(response, "Admin Panel")
 
         self.client.logout()
 
-        # Link for Stuff
-        self.client.login(username=self.staff.username,
-            password=self.staff.password)
+        # Hide for anonymous
+        response = self.client.get(reverse('gallery'))
+        self.assertEqual(response.status_code, 200)
+        self.assertNotContains(response, "Admin Panel")
+
+        # Show for Stuff
+        self.client.login(username=self.staff.username, password="pass")
 
         response = self.client.get(reverse('gallery'))
+        self.assertEqual(response.context["user"], self.staff)
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "Admin Panel")
 
         self.client.logout()
 
-        # Link for Admin
-        self.client.login(username=self.admin.username,
-            password=self.admin.password)
+        # Show for Admin
+        self.client.login(username=self.admin.username, password="pass")
 
         response = self.client.get(reverse('gallery'))
+        self.assertEqual(response.context["user"], self.admin)
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "Admin Panel")
 
